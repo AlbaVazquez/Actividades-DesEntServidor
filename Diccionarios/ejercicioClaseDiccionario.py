0c57aaf3495b14aaf3c13a184624c758e1a65510
AlbaVazquez <xavazgui498@ieshnosmachado.org>
2025-10-07 10:01:23 +0200

    a

--- Diccionarios/ejercicioClaseDiccionario.py
+++ Diccionarios/ejercicioClaseDiccionario.py
@@ -36,3 +36,38 @@ print("Lista de valoraciones:", listaValoraciones)
 print("La valoración media calculada con las nuevas listas es:", media2)
 
 
+
+
+cantidad = int(input("¿Cuántos videojuegos quieres introducir? "))
+
+for i in range(cantidad):
+    nombre = input("Nombre del videojuego: ")
+    valoracion = int(input("Valoración del videojuego (1-10): "))
+    categoria = input("Categoría del videojuego: ")
+
+    videojuego = {
+        "nombre": nombre,
+        "valoracion": valoracion,
+        "categoria": categoria
+    }
+    
+    videojuegos.append(videojuego)
+    
+print (videojuegos)
+
+for juego in videojuegos:
+    print(juego["nombre"])
+    totalV += juego["valoracion"]
+    
+media = totalV / cantidad
+print("La valoración media es:", media)
+
+#conseguir una lista con todos los nombres y otra con todas las valoraciones, calcular la media con 
+#la nueva lista de valoraciones. Realizar las listas en una sola línea de código.
+listaNombres = [juego["nombre"] for juego in videojuegos]
+listaValoraciones = [juego["valoracion"] for juego in videojuegos]
+media2 = sum(listaValoraciones) / len(listaValoraciones)
+
+print("Lista de nombres:", listaNombres)
+print("Lista de valoraciones:", listaValoraciones)
+print("La valoración media calculada con las nuevas listas es:", media2)
---
+++ Ficheros/primerEjemplo.py
@@ -0,0 +1,3 @@
+fichero = open("/home/alumno/Documentos/ejemplo.txt")
+fichero.read()
+fichero.close
